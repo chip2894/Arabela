164d64112c8acdb011139742385f7d19f2d82fc0
José Luis Castro <ing.joseluis.castro.serrato@gmail.com>
2025-10-17 10:31:43 -0600

    feat: identación de código

--- src/app/features/home/componentes/header/header.ts
+++ src/app/features/home/componentes/header/header.ts
@@ -2,26 +2,24 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  standalone: false,
-  templateUrl: './header.html',
-  styles: ``
+    selector: 'app-header',
+    standalone: false,
+    templateUrl: './header.html',
+    styles: ``,
 })
 export class Header {
-  //#region Variables
-  @Output() toggleSidebar = new EventEmitter<void>();
-  usuario = 'José Luis Castro Serrato';
-  //#endregion
+    //#region Variables
+    @Output() toggleSidebar = new EventEmitter<void>();
+    usuario = 'José Luis Castro Serrato';
+    //#endregion
 
-  constructor(private router: Router){
+    constructor(private router: Router) {}
 
-  }
+    goHome() {
+        this.router.navigate(['/home']);
+    }
 
-  goHome() {
-    this.router.navigate(['/home']);
-  }
-
-  logout() {
-    this.router.navigate(['/login']);
-  }
+    logout() {
+        this.router.navigate(['/login']);
+    }
 }
--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -3,53 +3,67 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  standalone: false,
-  templateUrl: './login.html',
-  styles: ``
+    selector: 'app-login',
+    standalone: false,
+    templateUrl: './login.html',
+    styles: ``,
 })
 export class Login implements OnInit {
+    //#region Variables
 
-  //#region Variables
+    loginForm!: FormGroup;
 
-  loginForm!: FormGroup;
+    //#endregion
 
-  //#endregion
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+    ) {}
 
-  constructor(private fb: FormBuilder, private router: Router){
-
-  }
-
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      usuario: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.maxLength(6)]]
-    });
-  }
-
-  get usuarioInvalid(): boolean {
-    return !!(this.loginForm.get('usuario') && this.loginForm.get('usuario')?.touched && this.loginForm.get('usuario')?.invalid);
-  }
-
-  get showUsuarioError(): boolean {
-    return !!(this.loginForm.get('usuario') && this.loginForm.get('usuario')?.touched && this.loginForm.get('usuario')?.invalid);
-  }
-
-  get passwordInvalid(): boolean {
-    return !!(this.loginForm.get('password') && this.loginForm.get('password')?.touched && this.loginForm.get('password')?.invalid);
-  }
-
-  get showPasswordError(): boolean {
-    return !!(this.loginForm.get('password') && this.loginForm.get('password')?.touched && this.loginForm.get('password')?.invalid);
-  }
-
-  onSubmit(): void{
-    if(this.loginForm.valid){
-        this.router.navigate(['']);
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            usuario: ['', [Validators.required]],
+            password: ['', [Validators.required, Validators.maxLength(6)]],
+        });
     }
-    else{
-      this.loginForm.markAllAsTouched();
-    }
-  }
 
+    get usuarioInvalid(): boolean {
+        return !!(
+            this.loginForm.get('usuario') &&
+            this.loginForm.get('usuario')?.touched &&
+            this.loginForm.get('usuario')?.invalid
+        );
+    }
+
+    get showUsuarioError(): boolean {
+        return !!(
+            this.loginForm.get('usuario') &&
+            this.loginForm.get('usuario')?.touched &&
+            this.loginForm.get('usuario')?.invalid
+        );
+    }
+
+    get passwordInvalid(): boolean {
+        return !!(
+            this.loginForm.get('password') &&
+            this.loginForm.get('password')?.touched &&
+            this.loginForm.get('password')?.invalid
+        );
+    }
+
+    get showPasswordError(): boolean {
+        return !!(
+            this.loginForm.get('password') &&
+            this.loginForm.get('password')?.touched &&
+            this.loginForm.get('password')?.invalid
+        );
+    }
+
+    onSubmit(): void {
+        if (this.loginForm.valid) {
+            this.router.navigate(['/home']);
+        } else {
+            this.loginForm.markAllAsTouched();
+        }
+    }
 }
